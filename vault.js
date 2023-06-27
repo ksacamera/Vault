2624e38009acf88b0bc403c9e92e5ec537f2e330
Kevin Anderson <ksacamera@gmail.com>
2023-06-27 13:36:11 -0700

    workshop2 jams

--- vault.js
+++ vault.js
@@ -1,4 +1,4 @@
-console.log("Vault Jams");
+console.log("Vault Jams")
 
 /*
 For this assignment: 
@@ -9,21 +9,17 @@ Vault Codes are: 10 - 40 - 39
 */
 
 /* The alert window will prompt the user with information */
-alert(
-  "You have received this message because you have been chosen to open an important vault. Here is the secret combination"
-);
+alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination")
 
 /* for the sequence below I assigned a value for each "num" */
-let num1 = 5 * 2; /* will show the number 10 */
+let num1 = 5 * 2 /* will show the number 10 */
 
-let num2 = 80 / 2; /* will show the number 20 */
+let num2 = 80 / 2 /* will show the number 20 */
 
-let num3 = 30 + 9; /* will show the number 39 */
+let num3 = 30 + 9 /* will show the number 39 */
 
 /* using the handy-dandy alert window again I displayed the vault combo by creating a string using the num nums and separated them using quotations */
-alert(
-  "One more click will reveal the vault combo and you'll be one step closer to committing Grand Larceny, you crook."
-);
-alert(num1 + " " + num2 + " " + num3 + " ");
+alert("One more click will reveal the vault combo and you'll be one step closer to committing Grand Larceny, you crook.")
+alert(num1 + " " + num2 + " " + num3 +" ")
 
 /* There you have it. We're both accessories to this crime. You're welcome. */
---
+++ vault2.js
@@ -0,0 +1,32 @@
+console.log("Vault Round 2")
+
+var person = prompt("Please enter you Burglar name", "Enter your name, crook");
+
+alert("Thank you, " + person + "." + " Your combo numbers are 10, 20, 39");
+
+
+if (confirm("Would you like to know the sum of those numbers?")) {
+    txt = "You pressed OK!";
+  } else {
+    txt = "You pressed Cancel!";
+  } 
+  
+let num1 = 5 * 2 
+
+let num2 = 40 / 2 
+  
+let num3 = 30 + 9 
+
+alert(num1  + num2  + num3);
+alert("Nice")
+
+
+
+
+
+// let text;
+// if (person == null || person == "") {
+//     text = "User cancelled the prompt.";
+// } else {
+//     text = "Hello " + person + "! How the hell are ya?";
+// }
