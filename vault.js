5809f349d749200fe252beeaa7c7e4bea8c3964e
Kevin Anderson <ksacamera@gmail.com>
2023-06-27 11:49:03 -0700

    first commit

---
+++ vault.js
@@ -0,0 +1,21 @@
+console.log("Vault Jams")
+
+/*
+For this assignment: 
+I am going to first build the string telling the user that they've been chosen to open an important vault.
+Next I will build the unique arithmetic operator that equals the codes in the combination
+Create a dialog box displaying the vault codes
+Vault Codes are: 10 - 40 - 39
+*/
+
+/* The alert window will prompt the user with information */
+alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination")
+
+let num1 = 5 * 2
+
+let num2 = 80 / 2
+
+let num3 = 30 + 9
+
+alert("One more click will reveal the vault combo and you'll be one step closer to committing Grand Larceny, you crook.")
+alert(num1 + " " + num2 + " " + num3 +" ")
