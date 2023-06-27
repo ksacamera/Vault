066478eaf7418132326ebc8b0aa4e72c2346a5de
Kevin Anderson <ksacamera@gmail.com>
2023-06-27 12:00:33 -0700

    added vault IMG and formatted

--- vault.js
+++ vault.js
@@ -1,4 +1,4 @@
-console.log("Vault Jams")
+console.log("Vault Jams");
 
 /*
 For this assignment: 
@@ -9,13 +9,21 @@ Vault Codes are: 10 - 40 - 39
 */
 
 /* The alert window will prompt the user with information */
-alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination")
+alert(
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination"
+);
 
-let num1 = 5 * 2
+/* for the sequence below I assigned a value for each "num" */
+let num1 = 5 * 2; /* will show the number 10 */
 
-let num2 = 80 / 2
+let num2 = 80 / 2; /* will show the number 20 */
 
-let num3 = 30 + 9
+let num3 = 30 + 9; /* will show the number 39 */
 
-alert("One more click will reveal the vault combo and you'll be one step closer to committing Grand Larceny, you crook.")
-alert(num1 + " " + num2 + " " + num3 +" ")
+/* using the handy-dandy alert window again I displayed the vault combo by creating a string using the num nums and separated them using quotations */
+alert(
+  "One more click will reveal the vault combo and you'll be one step closer to committing Grand Larceny, you crook."
+);
+alert(num1 + " " + num2 + " " + num3 + " ");
+
+/* There you have it. We're both accessories to this crime. You're welcome. */
